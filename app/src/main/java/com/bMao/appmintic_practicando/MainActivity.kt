ad7149b3b1ff76cbdfa97b1769a0da84bc4283d4
Mauricio <bmauricio.cruz07@gmail.com>
2021-11-07 19:19:55 -0500

    Base para mi proyecto de práctica appMóviles MinTIC

---
+++ app/src/main/java/com/bMao/appmintic_practicando/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bMao.appmintic_practicando
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
